1692f4da508a41d8c5b5152e85423f8350a6b496
Thandolwethu <thandwem045@gmail.com>
2024-11-22 10:14:34 +0200

    Added media queries

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 const buttons = document.querySelectorAll('[data-carousel-button]')
 
 buttons.forEach(
-    button => { 
-        button.addEventListener( "click", () => {
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1
-        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")    ;
-        const activeSlide = slides.querySelector("[data-active]");
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-        if (newIndex < 0) {
-            newIndex = slides.children.length - 1;
-        }
-        if ( newIndex >= slides.children.length) { newIndex = 0}
-        slides.children[newIndex].dataset.active = true;
-        delete activeSlide.dataset.active;
-   
+    button => {
+        button.addEventListener("click", () => {
+            const offset = button.dataset.carouselButton === "next" ? 1 : -1
+            const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+            const activeSlide = slides.querySelector("[data-active]");
+            let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+            if (newIndex < 0) {
+                newIndex = slides.children.length - 1;
+            }
+            if (newIndex >= slides.children.length) { newIndex = 0 }
+            slides.children[newIndex].dataset.active = true;
+            delete activeSlide.dataset.active;
+
+        })
     })
+
+// Nav link toggle
+
+const navToggle = document.querySelector('.hamburger');
+const modal = document.querySelector(".hamburger-modal")
+
+navToggle.addEventListener('click', () => {
+    if (modal.style.display === "none") {
+        modal.style.display = "block"
+    } else {
+        modal.style.display = "none";
+    }
+
 })
