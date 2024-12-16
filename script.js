4511840980fb2aae9be7979fadb5b0c31a13015b
Thando <thandwem045@gmail.com>
2024-12-16 16:30:10 +0200

    Completed project three

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 // Items from Dom
 
 const navToggle = document.querySelector('.hamburger');
-// KanBan Images
-const img1KTM = document.getElementById('img-1-KTM');
-const img2KTM = document.getElementById('img-2-KTM');
-const img3KTM = document.getElementById('img-3-KTM');
-
-// Van Life Images 
-const img1VL = document.getElementById('img-1-VL');
-const img2VL = document.getElementById('img-2-VL');
-const img3VL = document.getElementById('img-3-VL');
-
-// Cache Bank Landing Images 
-
-const img1CBL = document.getElementById('img-1-CBL');
-const img2CBL = document.getElementById('img-2-CBL');
-const img3CBL = document.getElementById('img-3-CBL');
 
 // Nav link toggle
 
